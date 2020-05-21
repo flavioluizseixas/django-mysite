4de00a535b623c6b121c12f7c93b9c0ffb454a9a
Flavio Seixas <flavio_seixas@id.uff.br>
2020-05-20 23:16:48 -0300

    v0

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('register/', views.signup, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.signup, name='register'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
